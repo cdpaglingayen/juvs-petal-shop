64bf9cac9a6ddb3ceff1350a766c72ef3e4ce582
Rainer Miguel <95733449+rendat@users.noreply.github.com>
2023-10-27 12:19:31 +0800

    Sheet DB and email integration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,9 +5,14 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
 use Illuminate\Mail\Message;
 use App\Mail\EmailExample;
+use SheetDB\SheetDB;
+use Carbon\Carbon;
+
+
 
 class UserController extends Controller
 {
+    
     public function index(Request $request)
     {
         $request->validate([
@@ -17,19 +22,36 @@ class UserController extends Controller
             'subject' => 'required',
             'message' => 'required',
         ]);
-          $email = 'petalsjuvs@gmail.com';
+    
+        $email = 'petalsjuvs@gmail.com';
+        $date = Carbon::now('GMT-7')->format('Y-m-d');
+    
         $data = [
-            'first_name' => $request->first_name,
-            'last_name' => $request->last_name,
-            'email' => $request->email,
-            'subject' => $request->subject,
-            'message' => $request->message,
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'subject' => $request->input('subject'),
+            'message' => $request->input('message'),
+            'date' => $date,
         ];
+
+      
+        $sheetdb = new SheetDB('wotxvq4mniu04');
+      
+        $success = $sheetdb->create($data);
     
-        Mail::to($email)
-            ->send(new EmailExample($data));
-    
-        return redirect()->back()->with('success', 'Email has been sent');
+        if ($success) {
+            Mail::to($email)->send(new EmailExample($data));
+            return redirect()->back()->with('success', 'Email has been sent sucessfully');
+        } else {
+            // Handle errors here if the data could not be saved to SheetDB.
+            return redirect()->back()->with('error', 'Failed to send email');
+        }
     }
     
+    
 }
+    
+    
+    
+
