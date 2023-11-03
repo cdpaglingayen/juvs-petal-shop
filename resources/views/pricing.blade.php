70432c128f2739edff1ea10da9251c537daf1f3f
RamPimentel <113711279+RamPimentel@users.noreply.github.com>
2023-11-03 13:00:39 +0800

    Update pricing.blade.php
    redirect Get Started link to doordash

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -10,7 +10,7 @@
             <hr class="border-2 border-white my-4">
             <h1 class="text-4xl font-bold">$99</h1>
             <p class="p-5">Flower bouquets carefully crafted with love and attention. We guarantee the freshest flowers at the best prices.</p>
-            <a href="#" class="bg-pink hover:bg-fuschia hover:text-white text-darkgray font-bold py-2 px-4 rounded-md mt-4">Get Started</a>
+            <a href="https://www.doordash.com/store/juvs-petals-san-diego-23504656/13531546/?fbclid=IwAR3A5J3r7saDAYnk16mfNjATfFY05m4UwU5ClsNB1GBt-2kJbszanUXyCqU" class="bg-pink hover:bg-fuschia hover:text-white text-darkgray font-bold py-2 px-4 rounded-md mt-4">Get Started</a>
         </div>
         <div class="relative text-center bg-darkgray p-5 rounded-2xl">
             <h5 class="text-2xl font-semibold text-pink">Advanced Service</h5>
@@ -18,14 +18,14 @@
             <h1 class="text-4xl font-bold text-pink">$199</h1>
             <p class="p-5">Elevate your floral experience with our premium selection of flower arrangements. Perfect for any special occasion.</p>
             <div class="absolute -top-6 left-1/2 transform -translate-x-1/2 bg-pink p-2.5 rounded z-10 md:text-xs md:w-36 sm:w-1/2 text-darkgray ">Most Popular <i class="bi bi-suit-heart-fill"></i></div>
-            <a href="#" class="bg-pink hover:bg-fuschia hover:text-white text-darkgray font-bold py-2 px-4 rounded-md mt-4">Get Started</a>
+            <a href="https://www.doordash.com/store/juvs-petals-san-diego-23504656/13531546/?fbclid=IwAR3A5J3r7saDAYnk16mfNjATfFY05m4UwU5ClsNB1GBt-2kJbszanUXyCqU" class="bg-pink hover:bg-fuschia hover:text-white text-darkgray font-bold py-2 px-4 rounded-md mt-4">Get Started</a>
         </div>
         <div class="relative text-center bg-darkgray p-5 rounded-2xl">
             <h5 class="text-2xl font-semibold">Intermediate Service</h5>
             <hr class="border-2 border-white my-4">
             <h1 class="text-4xl font-bold">$149</h1>
             <p class="p-5">Be amazed by our exclusive collection of rare and exotic blooms. These flowers will truly make a statement.</p>
-            <a href="#" class="bg-pink hover:bg-fuschia hover:text-white text-darkgray font-bold py-2 px-4 rounded-md mt-4">Get Started</a>
+            <a href="https://www.doordash.com/store/juvs-petals-san-diego-23504656/13531546/?fbclid=IwAR3A5J3r7saDAYnk16mfNjATfFY05m4UwU5ClsNB1GBt-2kJbszanUXyCqU" class="bg-pink hover:bg-fuschia hover:text-white text-darkgray font-bold py-2 px-4 rounded-md mt-4">Get Started</a>
         </div>
     </section>
 </section>
